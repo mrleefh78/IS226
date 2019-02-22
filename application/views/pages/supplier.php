a17ac3b615c18ddd32ea113b5786826c50d37a97
mrleefh78 <mrleefh78@gmail.com>
2019-02-22 19:32:08 +0400

    first version
    check first version

---
+++ application/views/pages/supplier.php
@@ -0,0 +1,47 @@
+<h2><?=$title?></h2>
+<table class="table table-hover">
+  <thead>
+    <tr>
+      <th></th>
+      <th></th>
+      <th scope="col">Supplier ID</th>
+      <th scope="col">Supplier Name</th>
+      <th scope="col">Address</th>
+      <th scope="col">Contact No</th>
+      <th scope="col">Contact Person</th>
+      <th scope="col">Email</th>
+    </tr>
+  </thead>
+  <tbody>
+  <tr class="table-secondary">
+    <td style="width: 50px;"><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-success btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><i class="fa fa-pencil fa-fw" aria-hidden="true"></i></button></p></td>
+    <td style="width: 50px;"><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete"><i class="fa fa-trash-o" aria-hidden="true"></i></button></p></td>
+      <td>S001</td>
+      <td>Al Muta</td>
+      <td>Oasis Road</td>
+      <td>221-3322</td>
+      <td>Hamad AlBalosi</td>
+      <td>HA@gmail.com</td>
+    </tr>
+    <tr class="table-default">
+    <td style="width: 50px;"><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-success btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><i class="fa fa-pencil fa-fw" aria-hidden="true"></i></span></button></p></td>
+    <td style="width: 50px;"><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete"><i class="fa fa-trash-o" aria-hidden="true"></i></span></button></p></td>
+      <td>S001</td>
+      <td>Al Muta</td>
+      <td>Oasis Road</td>
+      <td>221-3322</td>
+      <td>Hamad AlBalosi</td>
+      <td>HA@gmail.com</td>
+    </tr>
+    <tr class="table-secondary">
+    <td style="width: 50px;"><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-success btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><i class="fa fa-pencil fa-fw" aria-hidden="true"></i></button></p></td>
+    <td style="width: 50px;"><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete"><i class="fa fa-trash-o" aria-hidden="true"></i></button></p></td>
+      <td>S001</td>
+      <td>Al Muta</td>
+      <td>Oasis Road</td>
+      <td>221-3322</td>
+      <td>Hamad AlBalosi</td>
+      <td>HA@gmail.com</td>
+    </tr>
+  </tbody>
+</table> 
