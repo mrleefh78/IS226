dcddcce34350379f727aebd219b50d0c8c623675
Tessa <tessamadjus@gmail.com>
2019-03-03 23:47:22 +0300

    Update login.php

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -7,9 +7,6 @@
 <title>Login</title>
 
 
-<link rel="stylesheet" href = "https://bootswatch.com/4/yeti/bootstrap.min.css"/>
-        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
-
 
 <style>
 /* Basics */
