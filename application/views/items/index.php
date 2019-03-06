796902dc62ac8d2ae1c88dad8964a09676f6f429
Tessa <tessamadjus@gmail.com>
2019-03-06 21:30:34 +0300

    Update index.php

--- application/views/items/index.php
+++ application/views/items/index.php
@@ -12,11 +12,37 @@
       <th scope="col">Item Name</th>
       <th scope="col">Item Class</th>
       <th scope="col">Item Sub Class</th>
+      <th scope="col">Created by</th>
+      <th scope="col">Created Date</th>
+      <th scope="col">Updated by</th>
+       <th scope="col">Updated Date</th>
      </tr>
   </thead>
+  <tbody>
+  <?php foreach($items as $i) : ?>
+  <tr class="table-secondary">
  
+    <td style="width: 50px;"><p data-placement="top" data-toggle="tooltip" title="Edit" ><button class="btn btn-success btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" onclick="location.href='<?php echo base_url(); ?>items/edit/<?php echo ($i['item_id']); ?> ';"  ><i class="fa fa-pencil fa-fw" aria-hidden="true"></i></button></p></td>
+    <?php echo form_open('items/delete/' .$i['item_id']) ?>
+    
+    <td style="width: 50px;"><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete"><i class="fa fa-trash-o" aria-hidden="true"></i></button></p></td>
+    </form>
+      <td><?php echo $i['item_id']; ?></td>
+      <td><?php echo $i['item_name']; ?></td>
+      <td><?php echo $i['item_class_id']; ?></td>
+      <td><?php echo $i['item_sub_class_id']; ?></td>
+      <td><?php echo $i['created_by']; ?></td>
+      <td><?php echo $i['created_date']; ?></td>
+      <td><?php echo $i['updated_by']; ?></td>
+      <td><?php echo $i['updated_date']; ?></td>
+      
+      
+     
+    </tr>
+    <?php endforeach; ?>
+   
+  </tbody>
 </table> 
-
 </div>
 
 
