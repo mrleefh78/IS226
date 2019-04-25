d9416d1665e6411e36cf03fe12fdf54fffe52a02
mrleefh78 <47786356+mrleefh78@users.noreply.github.com>
2019-04-25 13:41:50 +0400

    latest copy

--- application/controllers/requests.php
+++ application/controllers/requests.php
@@ -11,4 +11,85 @@
 
         }
 
+        public function view($req_id = NULL){
+            
+           
+            $data['requests'] =  $this->request_model->get_requests($req_id);
+            if(empty($data['requests'])){
+                show_404();
+            }
+            $data['title'] = $data['requests']['req_id'];
+
+            $this->load->view('templates/header');
+            $this->load->view('requests/view', $data);
+            $this->load->view('templates/footer');
+
+        }
+
+       
+
+        public function create(){
+            
+            $data['title'] = 'Create Requests';
+
+            //$data['suppliers'] =  $this->supplier_model->get_suppliers();
+            $this->form_validation->set_rules('req_date','req_date','required');
+            $this->form_validation->set_rules('req_by','req_by','required');
+            $this->form_validation->set_rules('req_notes','req_notes','required');
+            // $this->form_validation->set_rules('address','address','required');
+            // $this->form_validation->set_rules('contact_no','contact_no','required');
+
+            if( $this->form_validation->run()===FALSE){
+                $this->load->view('templates/header');
+                $this->load->view('requests/create', $data);
+                $this->load->view('templates/footer');
+
+            }
+            else {
+                //$this->supplier_model->create_supplier();
+                // $this->load->view('suppliers/success');
+                //redirect('suppliers');
+
+                if ($this->request_model->create_request())
+                    {                             
+                        $this->session->set_flashdata('msg_success','Request created successfully!');
+                                
+                    }
+                    else
+                    {                
+                        $this->session->set_flashdata('msg_error','Error! Please try again later.');
+                        
+                    }
+
+                    redirect('requests');
+
+            }           
+
+        }
+
+       
+
+        public function edit($req_id = NULL){
+            $data['request'] =  $this->request_model->get_requests($req_id);
+            if(empty($data['request'])){
+                show_404();
+            }
+            // $data['title'] = $data['supplier']['supplier_id'];
+            $data['title'] = 'Update Request';
+
+            $this->load->view('templates/header');
+            $this->load->view('requests/edit', $data);
+            $this->load->view('templates/footer');
+        }
+
+        public function update($id){
+            $this->request_model->update_request($id);
+            redirect('requests');
+        }
+
+        public function delete($id){
+            $this->request_model->delete_request($id);
+            redirect('requests');
+        }
+
     }
--- application/views/requests/create.php
+++ application/views/requests/create.php
@@ -3,39 +3,36 @@
 <div class="col-md-8">
 <?php echo validation_errors(); ?>
 <?php $entries = array("name" => "createform");
-echo form_open('suppliers/create', $entries); ?>
+echo form_open('requests/create', $entries); ?>
   <fieldset>
     
   <div class="form-group">
-      <label for="supplier_name">Supplier Name</label>
-      <input type="text" class="form-control" id="supplier_name" name="supplier_name" aria-describedby="text" placeholder="Supplier Name">
+      <label for="req_date">Supplier Name</label>
+      <input type="text" class="form-control" id="req_date" name="req_date" aria-describedby="text" placeholder="Request Date">
       
     </div>
     <div class="form-group">
-      <label for="contact_person">Contact Person</label>
-      <input type="text" class="form-control" id="contact_person" name="contact_person" aria-describedby="text" placeholder="Contact Person">
+      <label for="department">Contact Person</label>
+      <input type="text" class="form-control" id="department" name="department" aria-describedby="text" placeholder="Department">
       
     </div>
+    
     <div class="form-group">
-      <label for="address">Address</label>
-      <textarea class="form-control" id="address" name="address"  rows="3"></textarea>
+      <label for="location">Contact Person</label>
+      <input type="text" class="form-control" id="location" name="location" aria-describedby="text" placeholder="Location">
+      
     </div>
+    
+    
 
     <div class="form-group">
-      <label for="contact_no">Contact no</label>
+      <label for="req_by">Contact no</label>
       <input type="text" class="form-control" id="contact_no" name="contact_no" aria-describedby="text" placeholder="Contact No">
       
     </div>
-   
     <div class="form-group">
-      <label for="email">Email address</label>
-      <input type="email" class="form-control" id="email" name="email"  aria-describedby="emailHelp" placeholder="Enter email">
-      
-    </div>
-    <div class="form-group">
-      <label for="website">Website</label>
-      <input type="text" class="form-control" id="website" name="website" aria-describedby="text" placeholder="Website">
-      
+      <label for="req_notes">Address</label>
+      <textarea class="form-control" id="address" name="address"  rows="3"></textarea>
     </div>
     
     <button type="submit" class="btn btn-primary">Submit</button>
